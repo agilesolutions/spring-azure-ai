c47319add0dc6ed28dc56f892f7003f9270f7c34
admin <rong@agile-solutions.ch>
2025-03-12 18:21:38 +0100

    integrate Twelvedata

--- src/main/java/com/agilesolutions/poc/service/ChatService.java
+++ src/main/java/com/agilesolutions/poc/service/ChatService.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -24,18 +25,22 @@ public class ChatService {
     public String getBestDeal(String version) {
 
         PromptTemplate pt = new PromptTemplate("""
-            {query}.
-            Which {target} is the most % growth?
+            Which share is the most % growth?
             The 0 element in the prices table is the latest price, while the last element is the oldest price.
+            Return a full name of company instead of a market shortcut. 
             """);
 
-        Prompt p = pt.create(
-                Map.of("query", "Find the most growth trends",
-                        "target", "share")
-        );
+        SearchRequest searchRequest = SearchRequest.builder()
+                .query("""
+            Find the most growth trends.
+            The 0 element in the prices table is the latest price, while the last element is the oldest price.
+            """)
+                .topK(3)
+                .similarityThreshold(0.7)
+                .build();
 
-        return aiClient.prompt(p)
-                .advisors(new QuestionAnswerAdvisor(store))
+        return aiClient.prompt(pt.create())
+                .advisors(new QuestionAnswerAdvisor(store, searchRequest))
                 .call()
                 .content();
 
